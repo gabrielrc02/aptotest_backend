3c05f4bcb1d9b5f0b624a3e512ae769231d6817a
gabri <gabrielramoscasas2a@gmail.com>
2026-08-09 20:46:57 +0200

    Prueba conexion bbdd

--- database.py
+++ database.py
@@ -1,14 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Le decimos que cree un archivo llamado 'aptotest.db' en esta misma carpeta
-URL_BASE_DATOS = "sqlite:///./aptotest.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Configuramos el motor (el check_same_thread es específico para que SQLite no de problemas en FastAPI)
-engine = create_engine(
-    URL_BASE_DATOS, connect_args={"check_same_thread": False}
-)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
